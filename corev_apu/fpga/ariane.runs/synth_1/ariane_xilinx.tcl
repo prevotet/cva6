84903af0a298bbbaac202f8f2ecda0d9c0eaf042
prevotet <jean-christophe.prevotet@insa-rennes.fr>
2026-03-26 00:02:49 +0100

    add rp_boundary_regs_mmu.sv

--- corev_apu/fpga/ariane.runs/synth_1/ariane_xilinx.tcl
+++ corev_apu/fpga/ariane.runs/synth_1/ariane_xilinx.tcl
@@ -296,6 +296,8 @@ read_verilog -library xil_defaultlib -sv {
   /home/jc/tmp/riscv-iommu-demo/cva6/corev_apu/fpga/src/ariane-ethernet/rgmii_soc.sv
   /home/jc/tmp/riscv-iommu-demo/cva6/corev_apu/rv_iommu/packages/rv_iommu/rv_iommu_reg_pkg.sv
   /home/jc/tmp/riscv-iommu-demo/cva6/corev_apu/rv_iommu/rtl/riscv_iommu.sv
+  /home/jc/tmp/riscv-iommu-demo/dpr/src/rp_boundary_regs.sv
+  /home/jc/tmp/riscv-iommu-demo/dpr/src/rp_boundary_regs_mmu.sv
   /home/jc/tmp/riscv-iommu-demo/cva6/vendor/pulp-platform/common_cells/src/rr_arb_tree.sv
   /home/jc/tmp/riscv-iommu-demo/cva6/vendor/pulp-platform/common_cells/src/rstgen.sv
   /home/jc/tmp/riscv-iommu-demo/cva6/vendor/pulp-platform/common_cells/src/rstgen_bypass.sv
