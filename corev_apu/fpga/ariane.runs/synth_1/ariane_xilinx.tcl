db1ee093cc7d379262524b67dc48f1ea38c901c2
jean-christophe.prevotet@insa-rennes.fr <jean-christophe.prevotet@insa-rennes.fr>
2026-03-25 17:51:41 +0100

    add accel

--- corev_apu/fpga/ariane.runs/synth_1/ariane_xilinx.tcl
+++ corev_apu/fpga/ariane.runs/synth_1/ariane_xilinx.tcl
@@ -71,7 +71,7 @@ proc create_report { reportName command } {
 }
 OPTRACE "synth_1" START { ROLLUP_AUTO }
 set_param checkpoint.writeSynthRtdsInDcp 1
-set_param synth.incrementalSynthesisCache ./.Xil/Vivado-111444-jc-VirtualBox/incrSyn
+set_param synth.incrementalSynthesisCache ./.Xil/Vivado-136726-jc-VirtualBox/incrSyn
 set_msg_config -id {Synth 8-256} -limit 10000
 set_msg_config -id {Synth 8-4480} -limit 1000
 set_msg_config -id {Synth 8-638} -limit 10000
